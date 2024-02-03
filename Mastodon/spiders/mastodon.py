50788d868a36f7bcc636da4be40f5c8ef50bafe5
Muhammad Muneeb ur Rehman <m.muneeb.ur.rehman.2000@gmail.com>
2024-02-03 22:27:13 +0500

    Selenium is addded to project for infinite scroll

--- Mastodon/spiders/mastodon.py
+++ Mastodon/spiders/mastodon.py
@@ -1,10 +1,29 @@
-import scrapy
+from scrapy import Spider
+from time import sleep
 
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-class MastodonSpider(scrapy.Spider):
+import os
+import requests
+import re
+import pandas as pd
+
+chrome_driver_path = r"D:\Projects\Assignments\DataScience\Web Scrapers\chromedriver.exe"
+
+class MastodonSpider(Spider):
     name = "mastodon"
     allowed_domains = ["mastodon.social"]
     start_urls = ["https://mastodon.social/explore"]
-
-    def parse(self, response):
-        pass
+     
+    def parse(self,response):
+        self.service = Service(chrome_driver_path)
+        self.driver = webdriver.Chrome(service=self.service)
+        self.driver.maximize_window()
+        self.driver.get("https://mastodon.social/explore")
+        
+        
+        self.driver.close()

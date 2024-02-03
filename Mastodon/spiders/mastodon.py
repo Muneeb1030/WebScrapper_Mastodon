18d3cc143b97a1903afe5f55e21f5009364bb10b
Muhammad Muneeb ur Rehman <m.muneeb.ur.rehman.2000@gmail.com>
2024-02-04 00:24:56 +0500

    News Scrapped along with saving in CSV and Downloading the Thumbnails

--- Mastodon/spiders/mastodon.py
+++ Mastodon/spiders/mastodon.py
@@ -27,17 +27,68 @@ class MastodonSpider(Spider):
         self.driver.get("https://mastodon.social/explore")
         self.Pages = self.driver.find_elements(By.XPATH,"//*[@class = 'account__section-headline']//a")
         
-        #self.CreateDirectories()
-        self.pasrse_HashTags(self.Pages[1])
+        self.CreateDirectories()
+        #self.pasrse_HashTags(self.Pages[1])
+        self.parse_News(self.Pages[2])
         
         self.driver.close()
 
+    
+    def parse_News(self,page):
+        self.driver.get(page.get_attribute("href"))
+        sleep(3)
+        
+        CSVPath = os.path.join('output_data\\News\\NewsData.csv')
+        
+        try:
+            df = pd.read_csv(CSVPath)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=['Publisher', 'PublishingDate','SharedInfo','Title','ThumbnailURL'])
+
+        News = self.driver.find_elements(By.CLASS_NAME, "story")
+        print(len(News))
+        new_data_list = []
+        
+        for news in News:
+            URL = news.find_element(By.XPATH, './/*[@class = "story__thumbnail"]//img').get_attribute('src')
+            Title = news.find_element(By.XPATH, './/*[@class = "story__details__title"]').text
+            try:
+                publishing_date = news.find_element(By.XPATH, './/*[@class = "story__details__publisher"]/time').get_attribute('title')
+            except:
+                publishing_date = date.today().strftime("%B %d, %Y")
+            new_data = {
+                'Publisher': news.find_element(By.XPATH, './/*[@class = "story__details__publisher"]/span').text,
+                'PublishingDate': publishing_date,
+                'SharedInfo': news.find_element(By.XPATH, './/*[@class = "story__details__shared"]').text,
+                'Title': Title,
+                'ThumbnailURL': URL
+            }
+            self.download_image(URL,Title,os.path.join('output_data\\News'))
+            new_data_list.append(new_data)
+            
+        if new_data_list:
+            df = pd.concat([df, pd.DataFrame(new_data_list)], ignore_index=True)
+        
+        df.to_csv(CSVPath, index=False,mode='a', header=not pd.io.common.file_exists(CSVPath))
+        self.driver.back()
+        sleep(3)    
+    
+    def download_image(self, image_url, Title,Path):
+        download = requests.get(image_url, stream=True)
+
+        Title = re.sub(r'[^\w\s]', '', Title)
+        
+        image_path = os.path.join(Path, f'{Title}.jpg')
+
+        with open(image_path, 'wb') as f:
+            for chunk in download.iter_content(chunk_size=128):
+                f.write(chunk)
     
     def pasrse_HashTags(self,page):
         self.driver.get(page.get_attribute("href"))
         sleep(3)
         
-        CSVPath = os.path.join('output_data\\HashTags.csv')
+        CSVPath = os.path.join('output_data\\Hashtag.csv')
         
         try:
             df = pd.read_csv(CSVPath)
@@ -62,17 +113,15 @@ class MastodonSpider(Spider):
         self.driver.back()
         sleep(3)
 
-
-
     def CreateDirectories(self):
         output_directory = 'output_data'
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
             
-        articles_sub_directory = f'{output_directory}/detailed_news_articles'
+        articles_sub_directory = f'{output_directory}/News'
         if not os.path.exists(articles_sub_directory):
             os.makedirs(articles_sub_directory)  
         
-        images_sub_directory = f'{output_directory}/news_articles_Images'
+        images_sub_directory = f'{output_directory}/Timeline'
         if not os.path.exists(images_sub_directory):
             os.makedirs(images_sub_directory) 
